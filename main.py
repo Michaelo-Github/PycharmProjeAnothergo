d768450ae34bf5fa0f28f9c978ad06c66e4f83a8
mo <michaelo@cjs.co.za>
2020-08-22 17:42:32 +0200

    done!

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import numpy as np
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -11,6 +13,11 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm!!!!!!')
+    print_hi('PyCharm!!!!!')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+    a = [1,2,3,4]
+    b = [4,5,6,7]
+
+    print(np.dot(a,b))
